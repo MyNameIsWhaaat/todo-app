8516d43c9bdeabb4a92e7365bec3f2e26f32efa2
Ekaterina Perminova <katanatroll@yandex.ru>
2025-01-31 17:07:27 +0400

    GetById, Update, Delete lists

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/MyNameIsWhaaat/todo-app"
 	"github.com/gin-gonic/gin"
@@ -58,13 +59,75 @@ func (h *Handler) getAllLists(c *gin.Context){
 }
 
 func (h *Handler) getListById(c *gin.Context){
-	
+    userId, err := getUserId(c)
+    if err != nil {
+        logrus.Errorf("failed to get user id: %s", err.Error())
+        return
+    }
+
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        newErrorResponce(c, http.StatusBadRequest, "invalid id param")
+    }
+
+    list, err := h.services.TodoList.GetById(userId, id)
+    if err != nil {
+        newErrorResponce(c, http.StatusInternalServerError, err.Error())
+        logrus.Errorf("failed to create todo list: %s", err.Error())
+        return
+    }
+
+    c.JSON(http.StatusOK, list)
 }
 
 func (h *Handler) updateList(c *gin.Context){
-	
+	userId, err := getUserId(c)
+    if err != nil {
+        logrus.Errorf("failed to get user id: %s", err.Error())
+        return
+    }
+
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        newErrorResponce(c, http.StatusBadRequest, "invalid id param")
+        return
+    }
+
+    var input todo.UpdateListInput
+    if err:= c.BindJSON(&input); err != nil{
+        newErrorResponce(c, http.StatusBadRequest, err.Error())
+        return
+    }
+
+    if err := h.services.Update(userId, id, input)
+    err != nil{
+        newErrorResponce(c, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    c.JSON(http.StatusOK, statusResponse{"Ok"})
 }
 
 func (h *Handler) deleteList(c *gin.Context){
-	
+    userId, err := getUserId(c)
+    if err != nil {
+        logrus.Errorf("failed to get user id: %s", err.Error())
+        return
+    }
+
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        newErrorResponce(c, http.StatusBadRequest, "invalid id param")
+    }
+
+    err = h.services.TodoList.Delete(userId, id)
+    if err != nil {
+        newErrorResponce(c, http.StatusInternalServerError, err.Error())
+        logrus.Errorf("failed to create todo list: %s", err.Error())
+        return
+    }
+
+    c.JSON(http.StatusOK, statusResponse{
+        Status: "Ok",
+    })
 }
